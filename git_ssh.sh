489c5442d1ffab7fab9b461849a6b376f3370f95
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-03-03 14:04:06 +0000

    Add ssh setup script

---
+++ git_ssh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+EMAIL=$1
+KEY_PATH="$HOME/.ssh/id_rsa"
+
+sudo apt update
+sudo apt install xclip -y
+
+ssh-keygen \
+    -t rsa \
+    -b 4096 \
+    -C "$EMAIL" \
+    -f "$KEY_PATH"
+
+eval "$(ssh-agent -s)"
+
+ssh-add "$KEY_PATH"
+
+xclip -sel clip < "$KEY_PATH.pub"
