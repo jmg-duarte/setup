e811720789321ea33b655391f776b2107ea510ba
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-02-23 18:27:36 +0000

    Create fisn_profile.sh

---
+++ fisn_profile.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+echo '#!/bin/bash -l' | sudo tee -a /usr/local/bin/fishlogin
+echo 'exec -l fish "$@"' | sudo tee -a /usr/local/bin/fishlogin
+chmod +x /usr/local/bin/fishlogin
+echo /usr/local/bin/fishlogin | sudo tee -a /etc/shells
+sudo usermod -s /usr/local/bin/fishlogin $USER
