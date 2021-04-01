7778bd755c89ca0f5c3b9a5b41320292377f28ec
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-04-02 00:08:12 +0100

    Update fish_profile.sh

--- fish_profile.sh
+++ fish_profile.sh
@@ -2,6 +2,6 @@
 
 echo '#!/bin/bash -l' | sudo tee -a /usr/local/bin/fishlogin
 echo 'exec -l fish "$@"' | sudo tee -a /usr/local/bin/fishlogin
-chmod +x /usr/local/bin/fishlogin
+sudo chmod +x /usr/local/bin/fishlogin
 echo '/usr/local/bin/fishlogin' | sudo tee -a /etc/shells
 sudo usermod -s /usr/local/bin/fishlogin $USER
